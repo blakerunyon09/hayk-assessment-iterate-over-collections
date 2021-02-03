fcc6706c2e92b6a9673f67b9cc5cad723a75f9e4
Blake Runyon <blakerunyon09@gmail.com>
2021-02-03 14:35:56 -0700

    Finished Companies Assessment

--- app.rb
+++ app.rb
@@ -13,4 +13,61 @@ companies = [
     Company.new('Delta', 3000)
 ]
 
-### Your code below this
+# 1. Generate a list of strings with the name and size (eg. "Alpha - 30")
+# pp companies_list companies
+
+def companies_list a
+    a.map.with_index { |el , i|
+        "#{i+1}. Name: #{el.name} - Size: #{el.size.to_s}"
+    }
+end
+
+# 2. Combine all the sizes with reduce
+# pp companies_total_size companies
+
+def companies_total_size a
+    a.inject(0) { |sum, el|
+        sum + el.size
+    }
+end
+
+# 3. Filter the list to show only companies over 100
+# pp companies_larger_than_X(companies, 100)
+
+def companies_larger_than_X a , size
+    companies_list(
+        a.select { |el|
+        el.size > size
+        }
+    )
+end
+
+# 4. Find the company named "Beta"
+# pp find_company_X companies, "Beta"
+
+def find_company_X a , name
+    companies_list( 
+        a.select { |el|
+           el.name == name
+        }
+    )
+end
+
+# 5. Find the largest company
+# pp find_largest_company companies
+
+def find_largest_company a
+    s = 0
+    a.map { |e|
+        biggest = e.name if e.size > s
+    }
+    biggest
+end
+
+# 6. Sort the companies from largest to smallest
+# pp sort_companies companies
+
+def sort_companies a
+    a.sort{ |a , b| b.size <=> a.size }
+end
+
